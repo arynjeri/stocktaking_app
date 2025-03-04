5ba0de31b24c8cac1f13edb4efa0f89c9c986afa
arynjeri <njeriary@gmail.com>
2025-03-04 12:22:30 +0300

    Added styling

--- login.php
+++ login.php
@@ -31,29 +31,34 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $stmt->close();
 }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Login</h1>
-
-    <?php if (isset($login_error)) : ?>
-        <p><?php echo $login_error; ?></p>
-    <?php endif; ?>
-
-    <form method="post" action="login.php">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required><br><br>
-
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required><br><br>
-
-        <input type="submit" value="Login">
-    </form>
-    <br>
-    <a href="forgot_password.php">Forgot Password?</a>
+    <div id="login_container">
+        <h1>Login</h1>
+        <?php if (isset($login_error)) : ?>
+            <p><?php echo $login_error; ?></p>
+        <?php endif; ?>
+        <form method="post" action="login.php" id="login_form">
+            <div class="row">
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username" required>
+            </div>
+            <div class="row">
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password" required>
+            </div>
+            <div class="button">
+                <input type="submit" value="Login">
+            </div>
+        </form>
+        <p class="forgot-password">
+            <a href="forgot_password.php">Forgot Password?</a>
+        </p>
+    </div>
 </body>
 </html>
